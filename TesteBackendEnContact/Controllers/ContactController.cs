ac99945ecfc01a65e6f37775f6c622d35c198acc
gabrielSoares522 <gabrielcsoares52@gmail.com>
2022-03-09 11:07:39 -0300

    upload .CSV

--- TesteBackendEnContact/Controllers/ContactController.cs
+++ TesteBackendEnContact/Controllers/ContactController.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Threading.Tasks;
 using TesteBackendEnContact.Controllers.Models;
 using TesteBackendEnContact.Core.Domain.ContactBook.Contact;
 using TesteBackendEnContact.Core.Interface.ContactBook.Contact;
 using TesteBackendEnContact.Repository.Interface;
+using Microsoft.AspNetCore.Hosting;
+using CsvHelper;
 
 namespace TesteBackendEnContact.Controllers
 {
@@ -14,7 +20,6 @@ namespace TesteBackendEnContact.Controllers
     public class ContactController : ControllerBase
     {
         private readonly ILogger<ContactController> _logger;
-
         public ContactController(ILogger<ContactController> logger)
         {
             _logger = logger;
@@ -42,6 +47,40 @@ namespace TesteBackendEnContact.Controllers
         public async Task<IContact> Get(int id, [FromServices] IContactRepository ContactRepository)
         {
             return await ContactRepository.GetAsync(id);
+        }
+        [HttpPost]
+        [Route("uploadCsv")]
+        public async Task<IActionResult> UploadCsv(IFormFile file, [FromServices] IContactRepository ContactRepository)
+        {
+            if (file.FileName.EndsWith(".csv"))
+            {
+                List<Contact> contacts = new List<Contact>();
+                using (var sreader = new StreamReader(file.OpenReadStream()))
+                {
+                    string[] headers = sreader.ReadLine().Split(',');
+                    while (!sreader.EndOfStream)
+                    {
+                        string[] rows = sreader.ReadLine().Split(',');
+                        int Id = int.Parse(rows[0].ToString());
+                        int ContactBookId = int.Parse(rows[1].ToString());
+                        int CompanyId = int.Parse(rows[2].ToString());
+                        string Name = rows[3].ToString();
+                        string Phone = rows[4].ToString();
+                        string Email = rows[5].ToString();
+                        string Address = rows[6].ToString();
+
+                        Contact contact = new Contact(Id,ContactBookId,CompanyId,Name,Phone,Email,Address);
+                        contacts.Add(contact);
+                        await ContactRepository.SaveAsync(contact);
+                    }
+                }
+            }
+            else
+            {
+                return BadRequest("the api only processes .csv files");  
+            }
+            return Ok();
+                //ContactRepository.GetAsync(id);
         }
     }
 }
